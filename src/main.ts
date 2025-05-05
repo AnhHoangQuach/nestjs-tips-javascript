54e87219ac5ee0cbe39a59e1b483f05b2c5aae31
AnhHoangQuach <hoanganh36.work@gmail.com>
2025-05-05 21:49:31 +0700

    feat: add logger dev

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { MyLogger } from 'src/logger/my.logger';
+import { AppModule } from './app.module';
+import { MyLoggerDev } from 'src/logger/my.logger.dev';
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    // logger: new MyLogger(),
+    bufferLogs: true,
+  });
+  app.useLogger(app.get(MyLoggerDev));
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors();
